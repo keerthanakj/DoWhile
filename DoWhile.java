4923e5683725ac91bdbc651def6245b43a601e7c
Keerthana KJ <87189032+keerthanakj@users.noreply.github.com>
2023-06-18 17:10:46 +0530

    Add files via upload

---
+++ DoWhile.java
@@ -0,0 +1,17 @@
+package MyPkg;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		do {
+			System.out.println("after while");
+		}
+		while(i>=1);
+		{
+			System.out.println("Hello");
+		}
+	}
+
+}
